bf2eb71f4b373ff9713763db28e83a34874aa1e3
Yoosy12121 <8ahmedyusuf8@gmail.com>
2025-02-09 09:15:27 +0000

    Add files via upload

---
+++ homework1/homework.py
@@ -0,0 +1,6 @@
+sample_dict={
+    "name":"jimbo",
+    "age": 120,
+    "city":"london"
+}
+print(sample_dict)
---
+++ homework1/homework2/homework2.py
@@ -0,0 +1,9 @@
+student_grades={"jimbo":90,"yusuf":100,"hi":40}
+jimbo_grade=student_grades["jimbo"]
+
+yusuf_grade=student_grades["yusuf"]
+
+hi_grade=student_grades["hi"]
+print(hi_grade)
+print(jimbo_grade)
+print(yusuf_grade)
